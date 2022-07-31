c1c9298080b24eb7e8a8ab06f64c825fd37a3c94
anilkumartarra <anilkumartarra@gmail.com>
2022-07-31 14:18:59 +0530

    g

---
+++ portfolio/script.js
@@ -0,0 +1,15 @@
+// Typing Animations
+
+var typed =new Typed(".typing",{
+    strings : ["Student","Player","Motivational-Speaker"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
+var typed =new Typed(".typing-2",{
+    strings : ["Student","Player","Motivational-Speaker"],
+    typeSpeed:99,
+    backSpeed:58,
+    loop:true
+})
